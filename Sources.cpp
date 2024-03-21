f98f5f4e4bd91ccd6799134ef18eadb5c7c0bb99
Ne0llix <william.darmon7@gmail.com>
2024-03-21 12:13:33 +0100

    Ajoutez des fichiers projet.

---
+++ Sources.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+//Debut -> Variable choix -> Variable Etage -> Variable Event Rdm -> Fin
+//Faire fonction combat avec (ATK, SOIN, ATK MAG, CHANT, FUITE)
+//Faire Inventaire en appuyant sur "E"
+//Fonction Cuisine en tapant "KOUIZINE"
+//Faire un GAME OVER
+
+int main() {
+	cout << "hello" << endl;
+	return 0;
+}
